a7aa65310e534484e8d46060e460da265170eb3a
Brandon Brasson <rocksong622@gmail.com>
2019-04-11 18:51:29 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,7 +6,7 @@ def create_project_hash
     kickstarter = Nokogiri::HTML(html)
   return  project = {}
 
-    
+
 
     kickstarter.css("li.project.grid_4").each do |project|
       title = project.css("h2.bbcard_name strong a").text
@@ -18,6 +18,6 @@ def create_project_hash
       }
     end
 
-    
+
     projects
   end
