9d9712b88c89a638a39120f46041dee881726a7b
Brandon Brasson <rocksong622@gmail.com>
2019-04-11 18:53:13 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,13 +1,13 @@
-# require libraries/modules here
+require 'nokogiri'
 
-def create_project_hash
-  # write your code here
+
+  def create_project_hash
     html = File.read('fixtures/kickstarter.html')
     kickstarter = Nokogiri::HTML(html)
 
-    project = {}
+    projects = {}
 
-
+    #Iterate through the projects
 
     kickstarter.css("li.project.grid_4").each do |project|
       title = project.css("h2.bbcard_name strong a").text
@@ -19,6 +19,6 @@ def create_project_hash
       }
     end
 
-
+    #Returns the projects hash
     projects
   end
