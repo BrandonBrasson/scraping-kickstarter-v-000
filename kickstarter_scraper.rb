a4eed8278856014a8be7743af6fdcece47d27eac
Brandon Brasson <rocksong622@gmail.com>
2019-04-11 18:57:12 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,9 +5,6 @@ require 'nokogiri'
     html = File.read('fixtures/kickstarter.html')
     kickstarter = Nokogiri::HTML(html)
 
-    projects = {}
-
-
     kickstarter.css("li.project.grid_4").each do |project|
       title = project.css("h2.bbcard_name strong a").text
       projects[title.to_sym] = {
@@ -18,6 +15,6 @@ require 'nokogiri'
         :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i }
     end
 
-
-
+    
+    projects
   end
