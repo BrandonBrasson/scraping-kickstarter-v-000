eccb2491454523b68bcfc9dc6e00f76f35956e77
Brandon Brasson <rocksong622@gmail.com>
2019-04-11 19:16:25 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,7 +7,7 @@ require 'nokogiri'
 projects = {}
   kickstarter.css("li.project.grid_4").each do |project|
   title = project.css("h2.bbcard_name strong a").text
-  projects[title.to_sym] = {
+  projects[title] = {
 #  projects[title] = {
 :location => project.css("span.location-name").text,
 
